79755dab55276e2199c6ab2b7624ddfd43819fae
Maurocazzato <maurizzio.125@gmail.com>
2023-11-16 15:54:54 +0100

    risultato domande js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// chiedere a user nome, cognome, colore per generare una password con risultato nome+cognome+colore+23
+
+// chiedo il nome
+const userName = parseInt(prompt("qual'è il tuo nome?"));
+console.log(userName);
+
+// chiedo il cognome
+const userSurname = parseInt(prompt("qual'è il tuo cognome?"));
+console.log(userSurname);
+
+// chiedo il colore preferito
+const userColor = parseInt(prompt("qual'è il tuo colore preferito?"));
+console.log(userColor);
+
+// ricevo la pssword finita
+let userPassword = userName + userSurname + userColor + "23"
+
+// output del risultato
+console.log(password);
+
+// risultato
+document.getElementById("risultato").innerHTML = "Ecco la tua nuova password: " + userPassword;
